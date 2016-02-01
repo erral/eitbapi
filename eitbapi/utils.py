8632f30cbde2c0e21df5290ad174312ac5061101
Mikel Larreategi <mlarreategi@codesyntax.com>
2016-02-01 12:43:03 +0100

    implementation with JSOn LD

--- eitbapi/utils.py
+++ eitbapi/utils.py
@@ -1,4 +1,5 @@
 EITB_FRONT_PAGE_URL = "http://www.eitb.tv/es/"
 EITB_EPISODE_LIST_REGEX = "<li[^>]*><a href=\"\" onclick\=\"setPlaylistId\('(\d+)','([^']+)','([^']+)'\)\;"
 EITB_PLAYLIST_BASE_URL = "http://www.eitb.tv/es/get/playlist/{}"
+EITB_VIDEO_BASE_URL = "http://www.eitb.tv/es/video/"
 EITB_VIDEO_URL = "http://www.eitb.tv/es/video/{}/{}/{}/{}/"
--- eitbapi/views.py
+++ eitbapi/views.py
@@ -4,6 +4,8 @@ from utils import EITB_FRONT_PAGE_URL
 from utils import EITB_EPISODE_LIST_REGEX
 from utils import EITB_PLAYLIST_BASE_URL
 from utils import EITB_VIDEO_URL
+from utils import EITB_VIDEO_BASE_URL
+
 
 import requests
 import re
@@ -20,6 +22,13 @@ def programs(request):
     data = requests.get(EITB_FRONT_PAGE_URL)
     matches = re.compile(EITB_EPISODE_LIST_REGEX, re.DOTALL).findall(data.text)
 
+    result = {
+        '@context': 'http://www.w3.org/ns/hydra/context.jsonld',
+        '@id': request.route_url('home'),
+        '@type': 'SiteRoot',
+        'parent': {},
+    }
+
     results = []
 
     for id, title1, title2 in matches:
@@ -27,47 +36,92 @@ def programs(request):
         if title1 != title2:
             scrapedtitle = scrapedtitle + " - " + title2
 
-        results.append(dict(
-            title=scrapedtitle,
-            id=id,
-        ))
+        results.append({
+            '@id': request.route_url('playlist', playlist_id=id),
+            'title': scrapedtitle,
+            'description': '',
+        })
 
-    return results
+    result['member'] = results
+
+    return result
 
 
 @view_config(route_name='playlist', renderer='prettyjson')
 def playlist(request):
     playlist_id = request.matchdict['playlist_id']
+    result = {
+        '@context': 'http://www.w3.org/ns/hydra/context.jsonld',
+        '@id': request.route_url('playlist', playlist_id=playlist_id),
+        '@type': 'Playlist',
+        'parent': request.route_url('programs'),
+    }
+
     playlist_url = EITB_PLAYLIST_BASE_URL.format(playlist_id)
     data = requests.get(playlist_url)
     playlist_data = data.json()
     web_medias = playlist_data.get('web_media')
-    playlist_data['web_media'] = []
-    new_web_medias = []
+    del playlist_data['web_media']
 
+    playlist_data['member'] = []
     for web_media in web_medias:
+        item = {
+            '@id': create_video_url(
+                playlist_data.get('name_playlist'),
+                playlist_data.get('id_web_playlist'),
+                web_media.get('NAME_ES'),
+                web_media.get('ID_WEB_MEDIA'),
+                add_domain=False,
+                request=request,
+            ),
+            'title': web_media.get('NAME_ES'),
+            'description': '',
+        }
+        playlist_data['member'].append(item)
+    del playlist_data['id']
 
-        web_media['videos'] = get_video_urls(
-            playlist_data.get('name_playlist'),
-            playlist_data.get('id_web_playlist'),
-            web_media.get('NAME_ES'),
-            web_media.get('ID_WEB_MEDIA'),
-        )
-        new_web_medias.append(web_media)
+    result.update(playlist_data)
+
+    return result
 
 
-    playlist_data['web_media'] = new_web_medias
-    return playlist_data
+@view_config(route_name='episode', renderer='prettyjson')
+def episode(request):
+    episode_url = request.matchdict['episode_url']
+    url = EITB_VIDEO_BASE_URL + episode_url
 
-
-def get_video_urls(playlist_title, playlist_id, video_title, video_id):
+    playlist_title, playlist_id, video_title, video_id = episode_url.split('/')
+    result = {
+        '@context': 'http://www.w3.org/ns/hydra/context.jsonld',
+        '@id': request.route_url('episode', episode_url=episode_url),
+        '@type': 'Episode',
+        'parent': request.route_url('playlist', playlist_id=playlist_id),
+    }
 
     ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+    video_data = ydl.extract_info(url, download=False)
+    
+    result.update(video_data)
+    return result
 
+
+def create_video_url(playlist_title, playlist_id, video_title, video_id, add_domain=True, request=None):
     playlist_title = playlist_title.lower().replace(' ', '-').replace('(', '').replace(')', '')
     video_title = playlist_title.lower().replace(' ', '-').replace('(', '').replace(')', '')
 
-    url = EITB_VIDEO_URL.format(playlist_title, playlist_id, video_id, video_title)
+    if add_domain:
+        url = EITB_VIDEO_URL.format(playlist_title, playlist_id, video_id, video_title)
+    else:
+        internal_url = '{}/{}/{}/{}'.format(playlist_title, playlist_id, video_id, video_title)
+        url = request.route_url('episode', episode_url=internal_url)
+    return url
+
+
+def get_video_urls(playlist_title, playlist_id, video_title, video_id):
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+    url = create_video_url(playlist_title, playlist_id, video_title, video_id)
+    import pdb; pdb.set_trace()
+
     result = ydl.extract_info(url, download=False)
 
     return result
