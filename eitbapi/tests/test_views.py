07c38ab9475da9bf329792a36ba8a7f2a3b9ef96
Mikel Larreategi <mlarreategi@codesyntax.com>
2016-12-06 13:33:52 +0100

    basic tests

---
+++ eitbapi/tests/test_views.py
@@ -0,0 +1,14 @@
+from pyramid import testing
+import unittest
+
+
+class FunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from eitbapi import main
+        app = main({})
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def test_root(self):
+        res = self.testapp.get('/', status=200)
+        self.assertTrue(b'Pyramid' in res.body)
