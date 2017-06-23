75ca20d7c34c7783013045078bb905744ce660b5
Mikel Larreategi <mlarreategi@codesyntax.com>
2017-06-23 16:52:57 +0200

    fix the regex to adapt to the new source HTML

--- eitbapi/utils.py
+++ eitbapi/utils.py
@@ -1,5 +1,5 @@
 EITB_FRONT_PAGE_URL = "http://www.eitb.tv/eu/"
-EITB_EPISODE_LIST_REGEX = "<li[^>]*><a href=\"\" onclick\=\"setPylstId\('(\d+)','([^']+)','([^']+)'\)\;"
+EITB_EPISODE_LIST_REGEX = "<li[^>]*>[^<]+<a href=\"\" onclick\=\"setPylstId\('(\d+)','([^']+)','([^']+)'\)\;"
 EITB_PLAYLIST_BASE_URL = "http://www.eitb.tv/es/get/playlist/{}"
 EITB_VIDEO_BASE_URL = "http://www.eitb.tv/es/video/"
 EITB_VIDEO_URL = "http://www.eitb.tv/es/video/{}/{}/{}/{}/"
