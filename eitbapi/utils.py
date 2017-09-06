267c0322cad930d496663607e9961578e5b9ca5f
Mikel Larreategi <mlarreategi@codesyntax.com>
2017-09-06 10:24:36 +0200

    python3 support

--- eitbapi/utils.py
+++ eitbapi/utils.py
@@ -1,8 +1,18 @@
+from __future__ import unicode_literals
+
 import requests
+import sys
 import xml.etree.ElementTree as ET
 
+if sys.version_info >= (3, 0, 0):
+    # for Python 3
+    PYTHON3 = True
+else:
+    PYTHON3 = False
+
+
 EITB_FRONT_PAGE_URL = "http://www.eitb.tv/eu/"
-EITB_EPISODE_LIST_REGEX = "<li[^>]*>[^<]*<a href=\"\" onclick\=\"setPylstId\('(\d+)','([^']+)','([^']+)'\)\;"
+EITB_EPISODE_LIST_REGEX = r"<li[^>]*>[^<]*<a href=\"\" onclick\=\"setPylstId\('(\d+)','([^']+)','([^']+)'\)\;" # noqa
 EITB_PLAYLIST_BASE_URL = "http://www.eitb.tv/es/get/playlist/{}"
 EITB_VIDEO_BASE_URL = "http://www.eitb.tv/es/video/"
 EITB_VIDEO_URL = "http://www.eitb.tv/es/video/{}/{}/{}/{}/"
@@ -15,13 +25,20 @@ EITB_RADIO_PROGRAM_LIST_XML_URL = 'http://www.eitb.tv/eu/menu/getMenu/radio/'
 
 
 def safe_unicode(text, charset='utf-8'):
-    if isinstance(text, unicode):
-        return text
+    if PYTHON3:
+        if isinstance(text, bytes):
+            return text
+    else:
+        if isinstance(text, unicode):
+            return text
     if isinstance(text, str):
         return unicode(text, charset)
     else:
         try:
-            return unicode(text)
+            if PYTHON3:
+                return bytes(text)
+            else:
+                return unicode(text)
         except:
             return ''
 
@@ -40,6 +57,7 @@ def xml_to_dict(data):
         d[dd['title']['text']] = dd
 
     return d
+
 
 def get_tv_program_data():
     results = []
@@ -62,6 +80,7 @@ def get_tv_program_data():
                 results.append(data)
 
     return results
+
 
 def get_radio_program_data():
     results = []
