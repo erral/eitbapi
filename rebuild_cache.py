6508d0d4b116597842ee25d6364398777c04241c
Mikel Larreategi <mlarreategi@codesyntax.com>
2020-01-17 09:12:29 +0100

    use absolute paths to build the cacje file

--- rebuild_cache.py
+++ rebuild_cache.py
@@ -1,13 +1,15 @@
 from eitbapi.utils import _get_tv_program_data
 
 import json
+import os
 
 
 def rebuild_cache():
+    cache_file = os.path.join(os.path.dirname(__file__), "cache.json")
     data = _get_tv_program_data()
-    with open('cache.json', 'w') as fp:
+    with open(cache_file, "w") as fp:
         json.dump(data, fp)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     rebuild_cache()
