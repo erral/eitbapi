dfc124f8e445832f5773d36817d9866d568e235f
Mikel Larreategi <mlarreategi@codesyntax.com>
2020-01-20 08:59:07 +0100

    update to pyramid 1.10 and python3-only

--- runapp.py
+++ runapp.py
@@ -5,6 +5,7 @@ from paste.script.cherrypy_server import cpwsgi_server
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    wsgi_app = loadapp('config:production.ini', relative_to='.')
-    cpwsgi_server(wsgi_app, host='0.0.0.0', port=port,
-                  numthreads=10, request_queue_size=200)
+    wsgi_app = loadapp("config:production.ini", relative_to=".")
+    cpwsgi_server(
+        wsgi_app, host="0.0.0.0", port=port, numthreads=10, request_queue_size=200
+    )
