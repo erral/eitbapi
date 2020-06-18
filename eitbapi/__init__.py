d380bfd8fafd35b23f5663758f5207ca17c68734
Mikel Larreategi <mlarreategi@codesyntax.com>
2020-06-18 16:24:56 +0200

    black

--- eitbapi/__init__.py
+++ eitbapi/__init__.py
@@ -10,38 +10,38 @@ from .cors import add_cors_to_response
 def main(global_config, **settings):
     """This function returns a Pyramid WSGI application."""
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
+    config.include("pyramid_chameleon")
 
-    config.add_directive(
-        'add_cors_preflight_handler', add_cors_preflight_handler)
-    config.add_route_predicate('cors_preflight', CorsPreflightPredicate)
+    config.add_directive("add_cors_preflight_handler", add_cors_preflight_handler)
+    config.add_route_predicate("cors_preflight", CorsPreflightPredicate)
 
-    config.add_subscriber(add_cors_to_response, 'pyramid.events.NewResponse')
+    config.add_subscriber(add_cors_to_response, "pyramid.events.NewResponse")
 
     config.add_route(
-        'cors-options-preflight', '/{catch_all:.*}',
-        cors_preflight=True,
+        "cors-options-preflight", "/{catch_all:.*}", cors_preflight=True,
     )
     config.add_view(
         cors_options_view,
-        route_name='cors-options-preflight',
+        route_name="cors-options-preflight",
         permission=NO_PERMISSION_REQUIRED,
     )
 
-    config.add_renderer('prettyjson', JSON(indent=4, sort_keys=True))
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('programs', '/playlist')
-    config.add_route('program-type-list', '/program-type-list')
-    config.add_route('program-type-news', '/program-type-news')
-    config.add_route('playlist', '/playlist/{playlist_id}')
-    config.add_route('playlist-per-type', '/type-playlist/{playlist_id}')
-    config.add_route('radio', '/radio')
-    config.add_route('radio-program-type-list', '/radio-program-type-list')
-    config.add_route('radio-playlist-per-type', '/type-rplaylist/{playlist_id}')
-    config.add_route('radio-stations', '/radio-stations')
-    config.add_route('radio-station-program-list', '/radio-station-programs/{station_id}')
-    config.add_route('radioplaylist', '/rplaylist/{playlist_id:[a-zA-Z0-9\.\-\/]+}')
-    config.add_route('episode', '/episode/{episode_url:[a-zA-Z0-9\.\-\/]+}')
+    config.add_renderer("prettyjson", JSON(indent=4, sort_keys=True))
+    config.add_static_view("static", "static", cache_max_age=3600)
+    config.add_route("home", "/")
+    config.add_route("programs", "/playlist")
+    config.add_route("program-type-list", "/program-type-list")
+    config.add_route("program-type-news", "/program-type-news")
+    config.add_route("playlist", "/playlist/{playlist_id}")
+    config.add_route("playlist-per-type", "/type-playlist/{playlist_id}")
+    config.add_route("radio", "/radio")
+    config.add_route("radio-program-type-list", "/radio-program-type-list")
+    config.add_route("radio-playlist-per-type", "/type-rplaylist/{playlist_id}")
+    config.add_route("radio-stations", "/radio-stations")
+    config.add_route(
+        "radio-station-program-list", "/radio-station-programs/{station_id}"
+    )
+    config.add_route("radioplaylist", "/rplaylist/{playlist_id:[a-zA-Z0-9\.\-\/]+}")
+    config.add_route("episode", "/episode/{episode_url:[a-zA-Z0-9\.\-\/]+}")
     config.scan()
     return config.make_wsgi_app()
--- eitbapi/tests/test_tv.py
+++ eitbapi/tests/test_tv.py
@@ -1,72 +1,84 @@
 import json
 import unittest
 
+
 class TVTests(unittest.TestCase):
     def setUp(self):
         from eitbapi import main
+
         app = main({})
         from webtest import TestApp
+
         self.testapp = TestApp(app)
 
     def test_programs(self):
-        res = self.testapp.get('/playlist', status=200)
-        self.assertTrue(res.headers.get('Content-type').startswith('application/json'))
+        res = self.testapp.get("/playlist", status=200)
+        self.assertTrue(res.headers.get("Content-type").startswith("application/json"))
 
     def test_program_playlist(self):
-        res = self.testapp.get('/playlist', status=200)
-        members = json.loads(res.text).get('member', [])
+        res = self.testapp.get("/playlist", status=200)
+        members = json.loads(res.text).get("member", [])
         for member in members[:2]:
             # Workaround to remove localhost prefix from id url
-            url = member.get('@id').replace('http://localhost', '')
+            url = member.get("@id").replace("http://localhost", "")
             result = self.testapp.get(url, status=200)
-            self.assertTrue(result.headers.get('Content-type').startswith('application/json'))
+            self.assertTrue(
+                result.headers.get("Content-type").startswith("application/json")
+            )
 
     def test_program_playlist_episode(self):
-        res = self.testapp.get('/playlist', status=200)
-        members = json.loads(res.text).get('member', [])
+        res = self.testapp.get("/playlist", status=200)
+        members = json.loads(res.text).get("member", [])
         for member in members[:1]:
             # Workaround to remove localhost prefix from id url
-            url = member.get('@id').replace('http://localhost', '')
+            url = member.get("@id").replace("http://localhost", "")
             result = self.testapp.get(url, status=200)
-            submembers = json.loads(result.text).get('member', [])
+            submembers = json.loads(result.text).get("member", [])
             for submember in submembers[:2]:
                 # Workaround to remove localhost prefix from id url
-                suburl = submember.get('@id').replace('http://localhost', '')
+                suburl = submember.get("@id").replace("http://localhost", "")
                 subresult = self.testapp.get(suburl, status=200)
-                self.assertTrue(subresult.headers.get('Content-type').startswith('application/json'))
+                self.assertTrue(
+                    subresult.headers.get("Content-type").startswith("application/json")
+                )
 
     def test_one_episode(self):
-        res = self.testapp.get('/playlist', status=200)
-        members = json.loads(res.text).get('member', [])
+        res = self.testapp.get("/playlist", status=200)
+        members = json.loads(res.text).get("member", [])
         for member in members[:1]:
             # Workaround to remove localhost prefix from id url
-            url = member.get('@id').replace('http://localhost', '')
+            url = member.get("@id").replace("http://localhost", "")
             result = self.testapp.get(url, status=200)
-            submembers = json.loads(result.text).get('member', [])
+            submembers = json.loads(result.text).get("member", [])
             for submember in submembers[:1]:
                 # Workaround to remove localhost prefix from id url
-                suburl = submember.get('@id').replace('http://localhost', '')
+                suburl = submember.get("@id").replace("http://localhost", "")
                 subresult = self.testapp.get(suburl, status=200)
-                files = json.loads(subresult.text).get('member', [])
+                files = json.loads(subresult.text).get("member", [])
                 for file in files:
-                    fileurl = file.get('@id').replace('http://localhost', '')
+                    fileurl = file.get("@id").replace("http://localhost", "")
                     fileresult = self.testapp.get(fileurl, status=200)
-                    self.assertTrue(fileresult.headers.get('Content-type').startswith('application/json'))
-
+                    self.assertTrue(
+                        fileresult.headers.get("Content-type").startswith(
+                            "application/json"
+                        )
+                    )
 
     def test_program_type_list(self):
-        res = self.testapp.get('/program-type-list', status=200)
-        self.assertTrue(res.headers.get('Content-type').startswith('application/json'))
+        res = self.testapp.get("/program-type-list", status=200)
+        self.assertTrue(res.headers.get("Content-type").startswith("application/json"))
 
     def test_program_type_news(self):
-        res = self.testapp.get('/program-type-news', status=200)
-        self.assertTrue(res.headers.get('Content-type').startswith('application/json'))
+        res = self.testapp.get("/program-type-news", status=200)
+        self.assertTrue(res.headers.get("Content-type").startswith("application/json"))
 
     def test_program_list_per_type(self):
-        res = self.testapp.get('/program-type-list', status=200)
-        members = json.loads(res.text).get('member', [])
+        res = self.testapp.get("/program-type-list", status=200)
+        members = json.loads(res.text).get("member", [])
         for member in members:
             # Workaround to remove localhost prefix from id url
-            url = member.get('@id').replace('http://localhost', '')
+            url = member.get("@id").replace("http://localhost", "")
             result = self.testapp.get(url, status=200)
-            self.assertTrue(result.headers.get('Content-type').startswith('application/json'))
+            self.assertTrue(
+                result.headers.get("Content-type").startswith("application/json")
+            )
