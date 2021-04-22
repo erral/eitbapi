3c8decd44c615e0d9d1ebb9c790a8c3cf708724a
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-04-22 09:56:27 +0200

    update requirements

--- runapp.py
+++ runapp.py
@@ -1,11 +1,9 @@
 import os
 
 from paste.deploy import loadapp
-from paste.script.cherrypy_server import cpwsgi_server
+from waitress import serve
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     wsgi_app = loadapp("config:production.ini", relative_to=".")
-    cpwsgi_server(
-        wsgi_app, host="0.0.0.0", port=port, numthreads=10, request_queue_size=200
-    )
+    serve(wsgi_app, host='0.0.0.0', port=port)
